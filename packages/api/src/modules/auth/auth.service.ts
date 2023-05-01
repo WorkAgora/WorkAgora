5234d911cfb2cab68f4ec757efccc1fb2aa4d903
Jean Cardonne <contact@jcardonne.com>
2023-05-01 11:58:23 +0200

    feat(auth): check if wallet already have a nonce to create a new one

--- packages/api/src/modules/auth/auth.service.ts
+++ packages/api/src/modules/auth/auth.service.ts
@@ -5,21 +5,43 @@ import { Auth, AuthKey } from './auth.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @InjectModel('Auth')
-    private readonly model: Model<Auth, AuthKey>
-  ) {}
+    constructor(
+        @InjectModel('Auth')
+        private readonly model: Model<Auth, AuthKey>
+    ) { }
 
-  public async getNonce(wallet: string): Promise<string> {
-    //@TODO: check if wallet already have a nonce to create a new one
-    const createdAuth = await this.model.create({
-      id: uuidv4(),
-      wallet: wallet,
-      nonce: uuidv4(),
-      nonceTimeout: new Date(Date.now() + 30 * 60 * 1000)
-    });
+    private async getAuthByWallet(wallet: string): Promise<Auth | null> {
+        try {
+            const auth = await this.model.query('wallet').eq(wallet).exec();
+            return auth.count ? auth[0] : null;
+        } catch (error) {
+            console.error('Error getting auth by wallet:', error);
+            return null;
+        }
+    }
 
-    // Return the generated nonce
-    return createdAuth.nonce;
-  }
+    public async getNonce(wallet: string): Promise<string> {
+        const existingAuth = await this.getAuthByWallet(wallet);
+        const now = new Date();
+
+        if (existingAuth && existingAuth.nonceTimeout > now) {
+            return existingAuth.nonce;
+        } else {
+            if (existingAuth) {
+                // Delete the expired nonce
+                await this.model.delete({ id: existingAuth.id });
+            }
+
+            // Create a new nonce
+            const createdAuth = await this.model.create({
+                id: uuidv4(),
+                wallet: wallet,
+                nonce: uuidv4(),
+                nonceTimeout: new Date(Date.now() + 30 * 60 * 1000),
+            });
+
+            // Return the generated nonce
+            return createdAuth.nonce;
+        }
+    }
 }
