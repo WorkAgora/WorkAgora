ac7b0d97afee1cc59323efc67277eb9911e61a76
Jean Cardonne <contact@jcardonne.com>
2023-05-02 17:05:50 +0200

    refactor(register): client profile and worker profile

--- packages/api/src/modules/user/user.interface.ts
+++ packages/api/src/modules/user/user.interface.ts
@@ -2,21 +2,30 @@ export interface UserKey {
   wallet: string;
 }
 
+export interface WorkerProfile {
+  skills?: string[];
+  situation?: string;
+  availability?: string;
+  hoursPerWeek?: number;
+  yearsOfExperience?: string;
+  certificates?: string[];
+  remuneration?: string;
+}
+
+export interface ClientProfile {
+  companyId?: string;
+}
+
 export interface User extends UserKey {
   email: string;
   description?: string;
   phone?: string;
   language?: string[];
-  skills?: string[];
-  situation?: string;
-  availability?: string;
-  hoursPerWeek?: number;
   location?: string;
-  remuneration?: string;
-  yearsOfExperience?: string;
-  certificates?: string[];
   profilePicture?: string;
   links?: string[];
   createdAt?: Date;
   updatedAt?: Date;
+  workProfile?: WorkerProfile;
+  clientProfile?: ClientProfile;
 }
