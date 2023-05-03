9ee55777342269e3d452b1a24be2384e16cb4a32
Jean Cardonne <contact@jcardonne.com>
2023-05-03 17:27:26 +0200

    feat(user-controller): getUser

--- packages/api/src/modules/user/user.controller.ts
+++ packages/api/src/modules/user/user.controller.ts
@@ -1,10 +1,49 @@
-import { Controller, Inject } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Inject, Get, Param, HttpException } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiParam, ApiResponse} from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { User } from './user.interface'
+import { UserDTO } from '../../dtos/user/user.dto';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   @Inject(UserService)
   private readonly userService: UserService;
+
+  @Get(':wallet')
+  @ApiOperation({ summary: 'Get user details by wallet address' })
+  @ApiParam({
+    name: 'wallet',
+    description: 'The wallet address of the user',
+    required: true,
+    schema: { type: 'string', default: '0x0' }
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The user details',
+    type: UserDTO
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'User not found'
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'An unexpected error occurred'
+  })
+  async getUser(@Param('wallet') wallet: string): Promise<User> {
+    try {
+      const user = await this.userService.findUserByWallet(wallet.toLowerCase());
+      if (!user) {
+        throw new Error('User not found');
+      }
+      return user;
+    } catch (e) {
+      if (e.message === 'User not found') {
+        throw new HttpException('User not found', 404);
+      } else {
+        throw new HttpException('An unexpected error occurred', 500);
+      }
+    }
+  }
 }
