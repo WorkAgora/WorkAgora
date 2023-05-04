962c2b1b06d7e9d9fb1a74d75bd288e1a120af8d
Jean Cardonne <contact@jcardonne.com>
2023-05-04 21:19:17 +0200

    refactor(ethsign): name of variable

--- packages/api/src/modules/ethers/ethers.service.ts
+++ packages/api/src/modules/ethers/ethers.service.ts
@@ -7,12 +7,12 @@ export class EthersService {
   @Inject(ConfigService)
   private readonly configService: ConfigService;
 
-  async signMessage(messages: { type: string; value: string }[]): Promise<string> {
+  async signMessage(messageDatas: { type: string; value: string }[]): Promise<string> {
     const privateKey = this.configService.get<string>('WALLET_PRIVATE_KEY');
     const wallet = new ethers.Wallet(privateKey);
 
-    const types = messages.map((message) => message.type);
-    const values = messages.map((message) => message.value);
+    const types = messageDatas.map((message) => message.type);
+    const values = messageDatas.map((message) => message.value);
 
     const messageHash = ethers.utils.solidityKeccak256(types, values);
     const signedMessage = await wallet.signMessage(ethers.utils.arrayify(messageHash));
