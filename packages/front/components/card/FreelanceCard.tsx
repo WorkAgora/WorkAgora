dddd6c79d4db0a551dcd861f31c713fb38272776
KonyTech <nicolas.cortella@protonmail.com>
2023-05-13 16:14:46 +0200

    fix: Fix landing and dashboard for freelance new card and illu change

--- packages/front/components/card/FreelanceCard.tsx
+++ packages/front/components/card/FreelanceCard.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Badge, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Badge, Box, Button, Flex, Text } from '@chakra-ui/react';
 import { FC } from 'react';
 import DollarIcon from '../icons/DollarIcon';
 import StarIcon from '../icons/StarIcon';
@@ -9,19 +9,23 @@ interface FreelanceCardProps {
   blurred?: boolean;
 }
 
+const skills = [
+  'Remote work', 'Full time', '3 months'
+]
+
 const FreelanceCard: FC<FreelanceCardProps> = ({ badges, blurred = false }: FreelanceCardProps) => {
   return (
     <Box
-      px={6}
-      py={8}
-      borderColor="brand.primary"
-      borderWidth="3px"
+      p={6}
+      borderColor="neutral.gray"
+      borderWidth="1px"
       borderRadius="32px"
+      bgColor="white"
       cursor="pointer"
       position="relative"
     >
       <Flex>
-        <Avatar w="56px" h="56px" />
+        <Avatar w="48px" h="48px" />
         <Flex flexDir="column" ml={4} justifyContent="center">
           <Text
             fontFamily="Comfortaa"
@@ -32,20 +36,76 @@ const FreelanceCard: FC<FreelanceCardProps> = ({ badges, blurred = false }: Free
           >
             John Doe
           </Text>
-          <Text
-            fontFamily="Comfortaa"
-            fontWeight="700"
-            fontSize="16px"
-            lineHeight="120%"
-            color="neutral.dsGray"
-          >
-            Freelance UI/UX Designer
-          </Text>
+          <Flex alignItems="center">
+            <Text
+              fontFamily="Montserrat"
+              fontWeight="400"
+              fontSize="14px"
+              lineHeight="150%"
+              color="neutral.black"
+            >
+              4,9/5
+            </Text>
+            <Box color="brand.primary" ml={1}>
+              <StarIcon />
+            </Box>
+          </Flex>
         </Flex>
       </Flex>
-      <Flex justifyContent="space-between" mt={4}>
+      <Flex flexDir="column" mt={4}>
+        <Text
+          fontFamily="Comfortaa"
+          fontWeight="700"
+          fontSize="16px"
+          lineHeight="120%"
+          color="neutral.black"
+        >
+          Freelance UI/UX Designer
+        </Text>
+        <Text
+          fontFamily="Comfortaa"
+          fontWeight="700"
+          fontSize="14px"
+          lineHeight="120%"
+          color="neutral.dsGray"
+        >
+          Rennes - France
+        </Text>
+      </Flex>
+      <Flex mt={2}>
+      {Array.from({ length: 3 }).map((_, k) => (
+          <Badge
+            key={k}
+            color='neutral.black'
+            bgColor='neutral.gray'
+            borderWidth="1px"
+            borderColor={'none'}
+            variant="filter"
+            mr={2}
+          >
+            {skills[k]}
+          </Badge>
+        ))}
+      </Flex>
+      <Flex mt={4} px={1}>
+        <Text
+          as="span"
+          fontFamily="Montserrat"
+          fontWeight="500"
+          fontSize="14px"
+          lineHeight="150%"
+          color="neutral.dsGray"
+        >
+          I specialize in creating visually appealing and user-friendly interfaces. With extensive
+          experience designing mobile app UI/UX, I can provide valuable insights and deliver
+          exceptional results. My portfolio showcases a range of successful projects, and my hourly
+          rate is competitive.
+        </Text>
+      </Flex>
+      <Flex mt={4}>
         {Array.from({ length: 4 }).map((_, k) => (
           <Badge
+            mr={2}
             key={k}
             color={badges[k].color}
             bgColor={badges[k].bgColor}
@@ -56,57 +116,12 @@ const FreelanceCard: FC<FreelanceCardProps> = ({ badges, blurred = false }: Free
             {badges[k].label}
           </Badge>
         ))}
-      </Flex>
-      <Flex mt={4} px={1} justifyContent="space-between">
-        <Flex alignItems="center">
-          <Text
-            fontFamily="Montserrat"
-            fontWeight="700"
-            fontSize="14px"
-            lineHeight="120%"
-            color="neutral.black"
-          >
-            4,9/5
-          </Text>
-          <Box color="brand.primary" ml={1}>
-            <StarIcon />
-          </Box>
-        </Flex>
-        <Flex alignItems="center">
-          <Flex w="20px" color="#38A169" justifyContent="center">
-            <DollarIcon />
-          </Flex>
-          <Text
-            ml={1}
-            fontFamily="Montserrat"
-            fontWeight="700"
-            fontSize="14px"
-            lineHeight="120%"
-            color="neutral.black"
-          >
-            40/hr
-          </Text>
-        </Flex>
-      </Flex>
-      <Flex mt={4} px={1}>
-        <Text
-          as="span"
-          fontFamily="Montserrat"
-          fontWeight="500"
-          fontSize="14px"
-          lineHeight="150%"
-          color="neutral.black"
-        >
-          I specialize in creating visually appealing and user-friendly interfaces. With extensive
-          experience designing mobile app UI/UX, I can provide valuable insights and deliver
-          exceptional results. My portfolio showcases a range of successful projects, and my hourly
-          rate is competitive.
-        </Text>
+        <Button ml='auto' variant='outline' px='12px !important' py='2px !important' bgColor="white" borderColor='neutral.gray' fontSize='14px' fontWeight='400' lineHeight='100%' maxH='26px'>See more</Button>
       </Flex>
       {blurred && (
         <Box
           position="absolute"
-          background="linear-gradient(180deg, rgba(217, 217, 217, 0) 30%, #EDF2F7 100%)"
+          background="linear-gradient(180deg, rgba(217, 217, 217, 0) 10%, #EDF2F7 100%)"
           w="102%"
           h="102%"
           top="-1%"
