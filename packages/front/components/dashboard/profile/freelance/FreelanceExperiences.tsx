265353a8fb2e2eb014d243f9cb39b6f040a3b496
LG <lgiffard44@gmail.com>
2023-05-28 01:13:30 +0200

    feat: End experiences add

--- packages/front/components/dashboard/profile/freelance/FreelanceExperiences.tsx
+++ packages/front/components/dashboard/profile/freelance/FreelanceExperiences.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Flex } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex } from '@chakra-ui/react';
 import { useCurrentUser } from '@workagora/front-provider';
 import AddIcon from '@workagora/front/components/icons/AddIcon';
+import { Experience } from '@workagora/utils';
 import { FC, useState } from 'react';
 import FreelanceExperienceForm from './FreelanceExperienceForm';
 import FreelanceExperiencesLine from './FreelanceExperiencesLine';
@@ -8,6 +9,12 @@ import FreelanceExperiencesLine from './FreelanceExperiencesLine';
 const FreelanceExperiences: FC = () => {
   const [showForm, setShowForm] = useState(false);
   const { user } = useCurrentUser();
+  const [curExperience, setCurExperience] = useState<Experience>();
+
+  const onEditExperience = (id: string) => {
+    setCurExperience(user?.freelanceProfile?.experiences?.find((v) => v.id === id));
+    setShowForm(true);
+  };
 
   return (
     <Flex
@@ -34,6 +41,7 @@ const FreelanceExperiences: FC = () => {
               </Box>
             }
             onClick={() => {
+              setCurExperience(undefined);
               setShowForm(true);
             }}
           >
@@ -41,14 +49,30 @@ const FreelanceExperiences: FC = () => {
           </Button>
         </Box>
       </Flex>
-      {showForm && <FreelanceExperienceForm onClose={() => setShowForm(false)} />}
+      {showForm && (
+        <FreelanceExperienceForm experience={curExperience} onClose={() => setShowForm(false)} />
+      )}
+      {showForm &&
+        user?.freelanceProfile?.experiences &&
+        user.freelanceProfile.experiences.length > 0 && <Divider borderColor="neutral.dsGray" />}
       {user &&
         user.freelanceProfile?.experiences &&
         user.freelanceProfile?.experiences?.length > 0 && (
-          <Flex flexDir="column">
-            {user.freelanceProfile.experiences.map((v, k) => (
-              <FreelanceExperiencesLine key={k} experience={v} />
-            ))}
+          <Flex flexDir="column" gap={4}>
+            {user.freelanceProfile.experiences
+              .sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime())
+              .map((v, k) => (
+                <>
+                  <FreelanceExperiencesLine
+                    key={k}
+                    experience={v}
+                    onEditExperience={onEditExperience}
+                  />
+                  {k !== user.freelanceProfile.experiences.length - 1 && (
+                    <Divider borderColor="neutral.dsGray" />
+                  )}
+                </>
+              ))}
           </Flex>
         )}
     </Flex>
